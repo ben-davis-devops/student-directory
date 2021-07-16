74656981e815e800a8a799c972ca2ccde6d88450
Ben Davis <ben.david.davis16@gmail.com>
2021-07-16 10:50:09 +0100

    list of students - updated

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# finally we orint the total number of students
+print "Overall, we have "
+# using print so we dont have a new line after each print
+print 11
+puts "great students"
